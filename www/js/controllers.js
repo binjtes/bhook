c561adeb83256417c9cc5fc5a09ffedb0e99ea52
binjtes <bintjes@gmail.com>
2016-01-02 19:42:53 +0100

    commit
    before changing to dotted ngmodels

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,246 +1,255 @@
 angular.module('bhook.controllers', ['bhook.services'])
 .controller('DashboardCtrl', function($scope,$http ,API_URL, bookService,$ionicModal,$translate ) {
-   bookService.initDB(API_URL);
-   $scope.submitData= {};
-   // the first skip for the latest book list is the first 8 already called by default
-   // purpose :TODO infinite scrolling
-   $scope.latestbooksSkip = 0;
-   // Create the auth/book distinction modal that is used before the insertion of data
-   $ionicModal.fromTemplateUrl('templates/addauthbookmodal.html',function($ionicModal) {
- 	    $scope.modalauthbook = $ionicModal;
-   }, {
-     scope: $scope
-   }).then(function(modalauthbook) {
-     $scope.modalauthbook = modalauthbook;
-   });
+	 bookService.initDB(API_URL);
+	 $scope.submitData= {};
+   $scope.formBookAuthorText = $translate.instant("add_a_book") ;
+  // $scope.data.formBookAuthorText = $translate.instant("add_a_book") ;
+	 // the first skip for the latest book list is the first 8 already called by default
+	 // purpose :TODO infinite scrolling
+	 $scope.latestbooksSkip = 0;
+	 // Create the auth/book distinction modal that is used before the insertion of data
+	 $ionicModal.fromTemplateUrl('templates/addauthbookmodal.html',function($ionicModal) {
+ 			$scope.modalauthbook = $ionicModal;
+	 }, {
+		 scope: $scope
+	 }).then(function(modalauthbook) {
+		 $scope.modalauthbook = modalauthbook;
+	 });
 
-   // Create the auth first and last distinction modal that is used before the insertion of data
-   $ionicModal.fromTemplateUrl('templates/addauthmodal.html',function($ionicModal) {
- 	    $scope.modalauth = $ionicModal;
-   }, {
-     scope: $scope
-   }).then(function(modalauth) {
-     $scope.modalauth = modalauth;
+	 // Create the auth first and last distinction modal that is used before the insertion of data
+	 $ionicModal.fromTemplateUrl('templates/addauthmodal.html',function($ionicModal) {
+ 			$scope.modalauth = $ionicModal;
+	 }, {
+		 scope: $scope
+	 }).then(function(modalauth) {
+		 $scope.modalauth = modalauth;
 
-   });
-
-
-   // close the AuthBook modal
-   $scope.closeAuthBookModal=  function(){
- 	  $scope.modalauthbook.hide();
-   }
-
-   // prepare author data
-   $scope.prepareAuthData = function(){
- 	  $scope.modalauthbook.hide();
- 	  // we need the firstname/lastname distinction for an author
- 	  $scope.openAuthModal($scope.submitData.author);
-   }
+	 });
 
 
-   // submitAuthBook
-   $scope.submitAuthBook = function(){
-   	 //  Object {book: "dqd", author_firstname: "sqsq", author_lastname: "" }
-   	  // remove unecessary fields
-   	  delete  $scope.submitData.author ;
-   	  // add timestamp
-   	  $scope.submitData['added'] = new Date();
-      // build the id using alphabetical order
-      $scope.submitData['_id'] = $scope.submitData['author_lastname']+'-'+$scope.submitData['author_firstname']+'-'+$scope.submitData['book'].toLowerCase();
-       // the id is the author name plus
-   	  console.log($scope.submitData) ;
-   	  //add the book
-   	  bookService.addBook($scope.submitData).then(function(book){
-      bookService.getLatestBooks().then(function(books){
-           $scope.latestbooks = books ;
-         });
-          $scope.modalauth.hide();
-          $scope.formBookAuthorText = $translate.instant("add_a_book") ;
-   	  });
-   }
-   // move text between input text to make author/book distinct before commit
-   $scope.movetext= function(authoronly){
+	 // close the AuthBook modal
+	 $scope.closeAuthBookModal=	function(){
+ 		$scope.modalauthbook.hide();
+	 }
 
- 	  if(authoronly){
+	 // prepare author data
+	 $scope.prepareAuthData = function(){
+ 		$scope.modalauthbook.hide();
+ 		// we need the firstname/lastname distinction for an author
+ 		$scope.openAuthModal($scope.submitData.author);
+	 }
 
- 		    // clean up double spaces, empty values
- 		    $scope.submitData.author_firstname = $scope.submitData.author_firstname.split(/[\s+]+/gm).filter(Boolean).join(" ");
- 		    $scope.submitData.author_lastname = $scope.submitData.author_lastname.split(/[\s+]+/gm).filter(Boolean).join(" ");
- 		    // count number of words
- 		    var firstnameln = $scope.submitData.author_firstname.split(/[\s+]+/gm).length ;
- 		    var lastnameln = $scope.submitData.author_lastname.split(/[\s+]+/gm).length ;
- 		    // additional filter
- 		    var fnarr = $scope.submitData.author_firstname.split(/[\s+]+/gm).filter(Boolean);
- 		    var lnarr = $scope.submitData.author_lastname.split(/[\s+]+/gm).filter(Boolean);
- 	        if (fnarr.length  > 0 &&  $scope.sensetogo == "right") {
- 	        	lnarr.unshift(fnarr[fnarr.length - 1]);
- 	        	fnarr.pop();
- 	        } else {
- 	             $scope.sensetogo = "left";
- 	            if (lnarr.length  > 0 &&  $scope.sensetogo == "left") {
- 	            	 fnarr.unshift(lnarr[lnarr.length - 1]);
- 	            	 lnarr.pop();
 
- 	            }else{
- 	             $scope.sensetogo = "right";
- 	            }
- 	        }
- 	        firstnameln = fnarr.length ;
- 	        lastnameln = lnarr.length ;
- 	        // restitute input value
- 	        $scope.submitData.author_firstname = fnarr.join(" ");
- 	        $scope.submitData.author_lastname = lnarr.join(" ");
- 	        // change arrow direction
- 	        if(firstnameln == 0 ){
- 	        	$scope.arrowDirectionClass = "ion-arrow-up-c";
- 	        }
- 	        if(lastnameln == 0 ){
- 	        	$scope.arrowDirectionClass = "ion-arrow-down-c";
- 	        }
- 		  return ;
- 	  }
- 	    // clean up double spaces, empty values
- 	    $scope.submitData.author = $scope.submitData.author.split(/[\s+]+/gm).filter(Boolean).join(" ");
- 	    $scope.submitData.book = $scope.submitData.book.split(/[\s+]+/gm).filter(Boolean).join(" ");
- 	    // count number of words
- 	    var authorln = $scope.submitData.author.split(/[\s+]+/gm).length ;
- 	    var bookln = $scope.submitData.book.split(/[\s+]+/gm).length ;
- 	    // additional filter
- 	    var autharr = $scope.submitData.author.split(/[\s+]+/gm).filter(Boolean);
- 	    var bookarr = $scope.submitData.book.split(/[\s+]+/gm).filter(Boolean);
-         if (autharr.length  > 0 &&  $scope.sensetogo == "right") {
-             bookarr.unshift(autharr[autharr.length - 1]);
-             autharr.pop();
-         } else {
-              $scope.sensetogo = "left";
-             if (bookarr.length  > 0 &&  $scope.sensetogo == "left") {
-                 autharr.unshift(bookarr[bookarr.length - 1]);
-                 bookarr.pop();
+	 // submitAuthBook
+	 $scope.submitAuthBook = function(){
+	 	 //	Object {book: "dqd", author_firstname: "sqsq", author_lastname: "" }
+	 		// remove unecessary fields
+	 		delete	$scope.submitData.author ;
+	 		// add timestamp
+	 		$scope.submitData['added'] = new Date();
+			// build the id using alphabetical order
+			$scope.submitData['_id'] = $scope.submitData['author_lastname']+'-'+$scope.submitData['author_firstname']+'-'+$scope.submitData['book'].toLowerCase();
+			 // the id is the author name plus
+	 		console.log($scope.submitData) ;
+	 		//add the book
+	 		bookService.addBook($scope.submitData).then(function(book){
+				  $scope.latestbooks.unshift($scope.submitData) ;
+					$scope.modalauth.hide();
+					console.log($scope.formBookAuthorText) ;
+					$scope.formBookAuthorText = $translate.instant("add_a_book") ;
 
-             }else{
-              $scope.sensetogo = "right";
-             }
-         }
-         authorln = autharr.length ;
-         bookln = bookarr.length ;
-         // restitute input value
-         $scope.submitData.author = autharr.join(" ");
-         $scope.submitData.book = bookarr.join(" ");
-         // change arrow direction
-         if(authorln==0 ){
-         	$scope.arrowDirectionClass = "ion-arrow-up-c";
-         }
-         if(bookln==0 ){
-         	$scope.arrowDirectionClass = "ion-arrow-down-c";
-         }
-   };
+					// erg .. does not refresh ?
+					console.log($scope.formBookAuthorText) ;
 
-   // open the AuthBookModal
-   $scope.openAuthBookModal=  function(formBookAuthorText){
- 	  $scope.modalauthbook.show();
- 	  $scope.arrowDirectionClass = "ion-arrow-down-c";
- 	  $scope.sensetogo = "right"
- 	  $scope.submitData.author= formBookAuthorText.split(/[\s+]+/gm).filter(Boolean).join(" "); ;
- 	  $scope.submitData.book = "" ;
-   }
+	 		});
+	 }
+	 // move text between input text to make author/book distinct before commit
+	 $scope.movetext= function(authoronly){
 
-   // open the AuthModal
-   $scope.openAuthModal=  function(scopesubmitDataauthor){
- 	  $scope.modalauth.show();
- 	  $scope.arrowDirectionClass = "ion-arrow-down-c";
- 	  $scope.sensetogo = "right" ;
- 	  $scope.submitData.author_firstname = scopesubmitDataauthor.split(/[\s+]+/gm).filter(Boolean).join(" "); ;
- 	  $scope.submitData.author_lastname = "" ;
-   }
+ 		if(authoronly){
 
-	// TODO : http://ionicframework.com/docs/api/directive/ionRefresher/ ? http://blog.ionic.io/pull-to-refresh-directive/
+ 				// clean up double spaces, empty values
+ 				$scope.submitData.author_firstname = $scope.submitData.author_firstname.split(/[\s+]+/gm).filter(Boolean).join(" ");
+ 				$scope.submitData.author_lastname = $scope.submitData.author_lastname.split(/[\s+]+/gm).filter(Boolean).join(" ");
+ 				// count number of words
+ 				var firstnameln = $scope.submitData.author_firstname.split(/[\s+]+/gm).length ;
+ 				var lastnameln = $scope.submitData.author_lastname.split(/[\s+]+/gm).length ;
+ 				// additional filter
+ 				var fnarr = $scope.submitData.author_firstname.split(/[\s+]+/gm).filter(Boolean);
+ 				var lnarr = $scope.submitData.author_lastname.split(/[\s+]+/gm).filter(Boolean);
+ 					if (fnarr.length	> 0 &&	$scope.sensetogo == "right") {
+ 						lnarr.unshift(fnarr[fnarr.length - 1]);
+ 						fnarr.pop();
+ 					} else {
+ 							 $scope.sensetogo = "left";
+ 							if (lnarr.length	> 0 &&	$scope.sensetogo == "left") {
+ 								 fnarr.unshift(lnarr[lnarr.length - 1]);
+ 								 lnarr.pop();
+
+ 							}else{
+ 							 $scope.sensetogo = "right";
+ 							}
+ 					}
+ 					firstnameln = fnarr.length ;
+ 					lastnameln = lnarr.length ;
+ 					// restitute input value
+ 					$scope.submitData.author_firstname = fnarr.join(" ");
+ 					$scope.submitData.author_lastname = lnarr.join(" ");
+ 					// change arrow direction
+ 					if(firstnameln == 0 ){
+ 						$scope.arrowDirectionClass = "ion-arrow-up-c";
+ 					}
+ 					if(lastnameln == 0 ){
+ 						$scope.arrowDirectionClass = "ion-arrow-down-c";
+ 					}
+ 			return ;
+ 		}
+ 			// clean up double spaces, empty values
+ 			$scope.submitData.author = $scope.submitData.author.split(/[\s+]+/gm).filter(Boolean).join(" ");
+ 			$scope.submitData.book = $scope.submitData.book.split(/[\s+]+/gm).filter(Boolean).join(" ");
+ 			// count number of words
+ 			var authorln = $scope.submitData.author.split(/[\s+]+/gm).length ;
+ 			var bookln = $scope.submitData.book.split(/[\s+]+/gm).length ;
+ 			// additional filter
+ 			var autharr = $scope.submitData.author.split(/[\s+]+/gm).filter(Boolean);
+ 			var bookarr = $scope.submitData.book.split(/[\s+]+/gm).filter(Boolean);
+				 if (autharr.length	> 0 &&	$scope.sensetogo == "right") {
+						 bookarr.unshift(autharr[autharr.length - 1]);
+						 autharr.pop();
+				 } else {
+							$scope.sensetogo = "left";
+						 if (bookarr.length	> 0 &&	$scope.sensetogo == "left") {
+								 autharr.unshift(bookarr[bookarr.length - 1]);
+								 bookarr.pop();
+
+						 }else{
+							$scope.sensetogo = "right";
+						 }
+				 }
+				 authorln = autharr.length ;
+				 bookln = bookarr.length ;
+				 // restitute input value
+				 $scope.submitData.author = autharr.join(" ");
+				 $scope.submitData.book = bookarr.join(" ");
+				 // change arrow direction
+				 if(authorln==0 ){
+				 	$scope.arrowDirectionClass = "ion-arrow-up-c";
+				 }
+				 if(bookln==0 ){
+				 	$scope.arrowDirectionClass = "ion-arrow-down-c";
+				 }
+	 };
+
+	 // open the AuthBookModal
+	 $scope.openAuthBookModal=	function(formBookAuthorText){
+ 		$scope.modalauthbook.show();
+ 		$scope.arrowDirectionClass = "ion-arrow-down-c";
+ 		$scope.sensetogo = "right"
+ 		$scope.submitData.author= formBookAuthorText.split(/[\s+]+/gm).filter(Boolean).join(" "); ;
+ 		$scope.submitData.book = "" ;
+	 }
+
+	 // open the AuthModal
+	 $scope.openAuthModal=	function(scopesubmitDataauthor){
+ 		$scope.modalauth.show();
+ 		$scope.arrowDirectionClass = "ion-arrow-down-c";
+ 		$scope.sensetogo = "right" ;
+ 		$scope.submitData.author_firstname = scopesubmitDataauthor.split(/[\s+]+/gm).filter(Boolean).join(" ");
+ 		$scope.submitData.author_lastname = "" ;
+		$scope.formBookAuthorText = $translate.instant("add_a_book") ;
+	 }
+
+	// TODO implement : http://ionicframework.com/docs/api/directive/ionRefresher/ ? http://blog.ionic.io/pull-to-refresh-directive/
 	 // populate latest book
-	  $scope.$on('$ionicView.enter', function(e) {
+		$scope.$on('$ionicView.enter', function(e) {
 			console.log("$ionicView.enter") ;
-		   //Refresh list TODO : avoid new request, store the info somehow
+			 //Refresh list when entering the view
+			 bookService.getLatestBooks().then(function(bookssortedbydate){
+	 				$scope.latestbooks = bookssortedbydate ;
+	 				 console.log(bookssortedbydate);
+	 		 });
+ 		});
 
 
- 	  });
-    bookService.getLatestBooks().then(function(bookssortedbydate){
-        $scope.latestbooks = bookssortedbydate ;
-         console.log(bookssortedbydate);
-     });
 
-
-		 // addbook click
+		 // addbook click - beginning of the input
 		 $scope.addBook = function(){
 			 // remove actual text from the input .
 			 $scope.formBookAuthorText = null;
-	  	};
+			};
 
-	  	$scope.submitAddBook = function(formBookAuthorText){
-	  		// show a modal dialog window to make final choice of ath/book book/auth surn/first, etc ..
-	  		$scope.openAuthBookModal(formBookAuthorText);
+			$scope.submitAddBook = function(formBookAuthorText){
+				// show a modal dialog window to make final choice of ath/book book/auth surn/firstname, to read true/false ..
+				$scope.openAuthBookModal(formBookAuthorText);
+			};
+/************* debug functions *****************/
+			$scope.testaddBook = function(){
 
-	  	};
+				console.log($scope.bookService);
+				var book2 = {
+					_id : "chinua-achebe-le-monde-s-effondre-"+ new Date().toString() ,
+					author_firstname:	"Chinua",
+					author_lastname: "Achebe",
+					book: "Le monde s'effondre" ,
+					added:	new Date() ,
+					to_read : true } ;
 
-      $scope.testaddBook = function(){
-        console.log($scope.bookService);
-        var book2 = {
-          _id : "chinua-achebe-le-monde-s-effondre",
-          author_firstname:  "Chinua",
-          author_lastname: "Achebe",
-          book: "Le monde s'effondre" ,
-          added:  new Date() ,
-          toread : true } ;
+					 return bookService.addBook(book2).then(function(book){
+								$scope.latestbooks.push(book2) ;
+								return true;
 
-         bookService.addBook(book2).then(function(book){
-             bookService.getLatestBooks().then(function(books){
-                console.log("from within function :new books added ?");
-                console.log(books);
-                $scope.latestbooks = books ;
-                console.log($scope.latestbooks );
-       		    });
-     	  });
-      };
-      $scope.resetdb = function(){
-      bookService.resetDb();
+		 				}).catch(function (err) {
+							 console.log(err);
+						 });
+			};
+			$scope.resetdb = function(){
+				bookService.resetDb();
+				// populate
+				var book1 = {
+					_id : "andersen-hans-christian-contes",
+					author_firstname: "Hans Christian",
+					author_lastname: "Andersen",
+					book: "Contes" ,
+					added:	"2015-12-13T17:33:02.276Z",
+					toread : true } ;
+					bookService.addBook(book1) ;
+					var book2 = {
+						_id : "chinua-achebe-le-monde-s-effondre",
+						author_firstname: "Chinua",
+						author_lastname: "Achebe",
+						book: "Le monde s'effondre" ,
+						added:	new Date() ,
+						toread : true } ;
 
-      // populate
-
-      var book1 = {
-        _id : "andersen-hans-christian-contes",
-        author_firstname: "Hans Christian",
-        author_lastname: "Andersen",
-        book: "Contes" ,
-        added:  "2015-12-13T17:33:02.276Z",
-        toread : true } ;
-        bookService.addBook(book1) ;
-        var book2 = {
-          _id : "chinua-achebe-le-monde-s-effondre",
-          author_firstname: "Chinua",
-          author_lastname: "Achebe",
-          book: "Le monde s'effondre" ,
-          added:  new Date() ,
-          toread : true } ;
-
-         bookService.addBook(book2) ;
-       }
+					 bookService.addBook(book2) ;
+		}
 
 
 
 })
 .controller('WishlistCtrl', function($scope,$http ,API_URL, bookService ) {
-  bookService.initDB(API_URL);
-
-	bookService.getBooksAlphabeticalOnAuthor().then(function(books){
-			 $scope.wishlist = books ;
-			 console.log(books) ;
+	bookService.initDB(API_URL);
 
 
-
+		$scope.$on('$ionicView.enter', function(e) {
+				bookService.getBooksAlphabeticalOnAuthor().then(function(books){
+						 $scope.wishlist = books ;
+						 console.log(books) ;
+					}).catch(function (err){
+							console.log(err);
+					});
 		});
-    $scope.deleteBook = function(index){
 
-      bookService.deleteBook($scope.wishlist[index]['_id']);
-           $scope.wishlist.splice(index, 1);
 
-    }
+		$scope.deleteBook = function(index){
+			bookService.deleteBook($scope.wishlist[index]['_id']).then(function(book){
+				console.log(book);
+				$scope.wishlist.splice(index, 1);
+
+			});
+
+
+		}
 
 
 
@@ -248,14 +257,14 @@ angular.module('bhook.controllers', ['bhook.services'])
 })
 .controller('AppCtrl', function($scope, $ionicModal, $timeout, $http ,API_URL, bookService ) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+	// With the new view caching in Ionic, Controllers are only called
+	// when they are recreated or on app start, instead of every page change.
+	// To listen for when this page is active (for example, to refresh data),
+	// listen for the $ionicView.enter event:
+	//$scope.$on('$ionicView.enter', function(e) {
+	//});
 
-  // Form data for the login modal
+	// Form data for the login modal
 
 
 
@@ -267,14 +276,14 @@ angular.module('bhook.controllers', ['bhook.services'])
 
 })
 .controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+	$scope.playlists = [
+		{ title: 'Reggae', id: 1 },
+		{ title: 'Chill', id: 2 },
+		{ title: 'Dubstep', id: 3 },
+		{ title: 'Indie', id: 4 },
+		{ title: 'Rap', id: 5 },
+		{ title: 'Cowbell', id: 6 }
+	];
 })
 .controller('PlaylistCtrl', function($scope, $stateParams) {
 });
--- www/js/services.js
+++ www/js/services.js
@@ -2,50 +2,45 @@ angular.module('bhook.services',[])
 .factory('bookService', ['$q', bookService  ]);
 
 function bookService($q){
- var _db ;
- var _books ;
- var _booksbydate ;
+  var _db ;
 
- var map = function(doc){
-   emit({added : doc.added , author_lastname: doc.author_lastname , author_firstname:doc.author_firstname, book:doc.book , _id:doc._id, toread:doc.toread});
- }
- var mapAuthLastName = function(doc){
-   emit({author_lastname: doc.author_lastname , author_firstname:doc.author_firstname, book:doc.book ,added : doc.added ,  _id:doc._id, toread:doc.toread});
- }
-
+  var map = function(doc){
+    emit({added : doc.added , author_lastname: doc.author_lastname , author_firstname:doc.author_firstname, book:doc.book , _id:doc._id, toread:doc.toread});
+  }
+  var mapAuthLastName = function(doc){
+    emit({author_lastname: doc.author_lastname , author_firstname:doc.author_firstname, book:doc.book ,added : doc.added ,  _id:doc._id, toread:doc.toread});
+  }
   return {
-	  initDB: function(API_URL){
-		  if(_db == undefined){
-	    	  console.log("instantiate DB locally and eventually " + API_URL);
-	    	  _db = new PouchDB('bhook', {adapter: 'websql'});
+	   initDB: function(API_URL){
+       if(_db == undefined){
+         console.log("instantiate DB locally and eventually " + API_URL);
+         _db = new PouchDB('bhook', {adapter: 'websql'});
 		 	   //PouchDB.debug.enable('*');
-	    	  PouchDB.debug.disable();
-	    	  // PouchDB.replicate('bhook', API_URL + '/bhook', {live: true});
-			 _db.info().then(function (info) {
-					  console.log(info);
-				})
-			// document that tells PouchDB/CouchDB
-			// to build up an index on doc.name
-			var ddoc = {
-			  _id: '_design/my_index',
-			  views: {
-			    by_name: {
-			      map: function (doc) { emit(doc.author_lastname); }.toString()
-			    },
-			    by_date: {
-				      map: function (doc) { emit(doc.added); }.toString()
-				},
-        by_toread: {
-            map: function (doc) { emit(doc.toread); }.toString()
-       }
-
-
+         PouchDB.debug.disable();
+         // PouchDB.replicate('bhook', API_URL + '/bhook', {live: true});
+  			 _db.info().then(function (info) {
+            console.log(info);
+  				})
+    			// document that tells PouchDB/CouchDB
+    			// to build up an index on doc.name
+          var ddoc = {
+            _id: '_design/my_index',
+            views: {
+              by_name: {
+                map: function (doc) { emit(doc.author_lastname); }.toString()
+              },
+              by_date: {
+                map: function (doc) { emit(doc.added); }.toString()
+              },
+              by_toread: {
+                map: function (doc) { emit(doc.toread); }.toString()
+              }
 			  }
 			};
 			// save it
-			 _db.put(ddoc).then(function () {
-			  // success!
-				 console.log('index created') ;
+			_db.put(ddoc).then(function () {
+			// success!
+      console.log('index created') ;
 			}).catch(function (err) {
 				 console.log('index already created') ;
 			  // some error (maybe a 409, because it already exists?)
@@ -54,26 +49,28 @@ function bookService($q){
 		  }
 		} ,
 		getLatestBooks: function(skip){
-			return $q.when(_db.query( map ,{
-        descending : true,
-        skip: skip ,
-        limit : 20
-      }))
-        .then(function(books) {
-          console.log('entering query');
-	    			console.log(books);
-              console.log('**entering query');
-	    			_booksbydate = books.rows.map(function(row) {
-              console.log("row:");
-              console.log(row);
-	    				return row['key'];
-	          });
-            console.log(_booksbydate);
-	          return _booksbydate;
-	    	}).catch(function (err) {
-	    		console.log(err);
-	    	});
+      // only query once
 
+      			return $q.when(_db.query( map ,{
+              descending : true,
+              skip: skip ,
+              limit : 20
+            }))
+              .then(function(books) {
+                console.log('entering query');
+      	    			console.log(books);
+                  console.log('**entering query');
+                  var booksbydate ;
+      	    			booksbydate = books.rows.map(function(row) {
+                    console.log("row:");
+                    console.log(row);
+      	    				return row['key'];
+      	          });
+
+      	          return booksbydate;
+      	    	}).catch(function (err) {
+      	    		console.log(err);
+      	    	});
 
 		},
     getBooksAlphabeticalOnAuthor: function(){
@@ -83,39 +80,36 @@ function bookService($q){
           console.log('entering query');
 	    			console.log(books);
               console.log('**entering query');
-	    			_booksbyauthorname = books.rows.map(function(row) {
+              var booksbyauthorname ;
+	    			  booksbyauthorname = books.rows.map(function(row) {
               console.log("row:");
               console.log(row);
 	    				return row['key'];
 	          });
-            console.log(_booksbydate);
-	          return _booksbyauthorname;
+
+	          return booksbyauthorname;
 	    	}).catch(function (err) {
 	    		console.log(err);
 	    	});
 
 
 		},
-
-
-
 		getAllBooks: function(){
 	    	var options =   {descending : true,include_docs: true } ;
 	    	return $q.when(_db.allDocs(options))
 	    	.then(function(books) {
 	    			console.log(books);
-	    			_books = books.rows.map(function(row) {
-
+            var books ;
+	    			books = books.rows.map(function(row) {
 	    				return row['doc'];
 	          });
-	          return _books;
+	          return books;
 	    	}).catch(function (err) {
 	    		console.log(err);
 	    	});
 		},
 
 		getBooks: function(){
-			if (!_books) {
 					return $q.when(_db.allDocs({ include_docs: true}))
 					.then(function(books) {
 						console.log(books);
@@ -126,12 +120,6 @@ function bookService($q){
 				 }).catch(function (err) {
 				  console.log(err);
 				});
-				;
-
-			} else {
-		        // Return cached data as a promise
-		        return $q.when(_books);
-			}
 	},
 
    getBooksWishList: function(){
@@ -146,10 +134,7 @@ function bookService($q){
           }).catch(function (err) {
            console.log(err);
          });
-
-         ;
-
-     } else {
+        } else {
          // Return cached data as a promise
          return $q.when(_books);
      }
@@ -158,24 +143,29 @@ function bookService($q){
 		getBook: function(){
 			  return $http.get(API_URL+'/book').then(function(resp) {
 				    return resp.data;
-				  }, function(err) {
+				  }).catch (function (err) {
 				    console.error('ERR', err);
 
-				 })
+				 });
 		},
     deleteBook: function(idbbook){
-         _db.get(idbbook).then(function(doc) {
-             _db.remove(doc);
-         }).then(function (result) {
-           // handle result
-           return result;
-         }).catch(function (err) {
-           console.log(err);
-         });
+         return $q.when(_db.get(idbbook).then(function(doc) {
+              return _db.remove(doc);
+           }).catch(function (err) {
+             console.log(err);
+           })
+       );
     	},
 		addBook: function(submitData){
 				console.log("about to submit ")
-				return $q.when(_db.put(submitData));
+        console.log(submitData);
+        return $q.when(_db.put(submitData)).then(function(addedbook) {
+               return addedbook;
+          // body...
+        }).catch(function (err) {
+           console.log(err);
+         });
+
 
 		},
     resetDb : function(){
